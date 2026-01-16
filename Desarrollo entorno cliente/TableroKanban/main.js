549e453f82e159786e005c6a11af7db06e6ba013
Manu <mmd0012@alu.medac.es>
2026-01-16 10:31:03 +0100

    Funciones varias

--- Desarrollo entorno cliente/TableroKanban/main.js
+++ Desarrollo entorno cliente/TableroKanban/main.js
@@ -1,5 +1,7 @@
 const inputNum = document.getElementById("num");
-const contenido = document.getElementById("tabla")
+const contenido = document.getElementById("tabla");
+const form = document.getElementById("formulario");
+
 
 function obtener (){
     const datos = localStorage.getItem("tablero");
@@ -14,10 +16,28 @@ function guardar(arrayDatos){
     localStorage.setItem("tablero", JSON.stringify(arrayDatos));
 }
 
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const cant = parseInt(inputNum.value);
+
+    for (let i = 0; i <cantidad; i++){
+        const div = document.createElement("div");
+
+        const inputNombre = document.createElement("input");
+        inputNombre.type = "text";
+        inputNombre.placeholder = "Nombre: ";
+    }
+})
+
 function mostrarTabla(){
     contenido.innerHTML= "";
     const tablaActivo = obtener();
 
-    tablaActivo.forEach(())
+    tablaActivo.forEach((texto, indice) =>{
+        const tab = document.createElement("tablero");
+        tab.textContent = texto;
+
+        
+    })
 }
 
